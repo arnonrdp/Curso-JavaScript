22e6281ca1f11cef8028e4a15ca0dcb30096b9de
Arnon Rodrigues <arnonrdp@gmail.com>
2020-10-09 13:51:00 -0300

    Exercício 008

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A sua velocidade é de ${vel}Km/h`)
+if (vel > 60) { //Condição Simples
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança.`)
