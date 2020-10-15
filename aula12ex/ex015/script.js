f1b43902eb444f9634fe38c59eca799d96aa63bc
Arnon Rodrigues <arnonrdp@gmail.com>
2020-10-15 13:51:00 -0300

    Exercício 015

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,56 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+
+        var img = document.createElement('img') //Criar elemento no HTML
+        img.setAttribute('id', 'foto') //Definir o id deste elemento
+
+        if (fsex[0].checked) {
+            genero = 'homem'
+            if (idade >= 0 && idade <= 10) {
+                //Criança
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 20) {
+                //Jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 60) {
+                //Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else {
+            genero = 'mulher'
+            if (idade >= 0 && idade <= 10) {
+                //Criança
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 20) {
+                //Jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 60) {
+                //Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} de ${idade} anos.`
+        res.appendChild(img) // Adicionar um elemento (no caso: img)
+
+        if (idade > 150) {
+            res.innerHTML = `Essa pessoa de ${idade} anos provavelmente já faleceu.`
+        } 
+    }
+}
